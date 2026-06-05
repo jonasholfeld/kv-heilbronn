a13210b689ee83aeb1527fccbd9870fd67f56212
Jonas Holfeld <klatschpauke@googlemail.com>
2026-06-05 16:01:16 +0200

    local changes

--- site/config/config.php
+++ site/config/config.php
@@ -1,7 +1,11 @@
 <?php
 
 $debug = getenv('KIRBY_DEBUG');
+$host = $_SERVER['HTTP_HOST'] ?? '';
+$isLocalHost = in_array($host, ['127.0.0.1:8000', 'localhost:8000', '127.0.0.1', 'localhost'], true);
 
 return [
-    'debug' => $debug !== false && filter_var($debug, FILTER_VALIDATE_BOOL),
+    'debug' => $debug !== false
+        ? filter_var($debug, FILTER_VALIDATE_BOOL)
+        : $isLocalHost,
 ];
